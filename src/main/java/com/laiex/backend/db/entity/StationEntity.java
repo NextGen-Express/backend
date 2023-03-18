b603d02e0c15b67509cc91b00c1b5a36a99c1b9d
yixiuzhu <zhuyixiu@bu.edu>
2023-03-18 11:28:55 -0700

    Implementation of station entity

---
+++ src/main/java/com/laiex/backend/db/entity/StationEntity.java
@@ -0,0 +1,18 @@
+package com.laiex.backend.db.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("stations")
+public record StationEntity(
+        @Id Long id,
+        String address,
+        String city,
+        String country,
+        Float latitude,
+        Float longitude,
+        String status
+) {
+}
+
