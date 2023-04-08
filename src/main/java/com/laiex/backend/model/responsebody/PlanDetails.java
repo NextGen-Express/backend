31baabc107550b2677ff6f622378e9bfca99b447
John Zhang <ZhangZ1107@gmail.com>
2023-04-07 20:24:39 -0700

    search response update

--- src/main/java/com/laiex/backend/model/responsebody/PlanDetails.java
+++ src/main/java/com/laiex/backend/model/responsebody/PlanDetails.java
@@ -5,7 +5,7 @@ import com.laiex.backend.db.entity.CarrierEntity;
 import java.time.LocalDateTime;
 
 public record PlanDetails(
-        Long carrierId,
+        String stationAddress, Long carrierId,
         CarrierEntity.CarrierType carrierType,
         String origin,
         String destionation,
@@ -13,6 +13,5 @@ public record PlanDetails(
         Double weight,
         LocalDateTime estimatedPickTime,
         LocalDateTime estimatedDeliveryTime,
-        Double price,
-        com.google.maps.model.DirectionsRoute route) {
+        Double price) {
 }
